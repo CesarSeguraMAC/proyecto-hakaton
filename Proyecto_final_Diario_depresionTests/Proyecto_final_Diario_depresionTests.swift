3f58e84420dd4c88ba9b85d8479673c8b761e5a8
Jorge Alberto de Jesus Cortinas Avila <81351458+JorgeCortinas@users.noreply.github.com>
2024-11-06 02:38:35 -0600

    Initial Commit

---
+++ Proyecto_final_Diario_depresionTests/Proyecto_final_Diario_depresionTests.swift
@@ -0,0 +1,17 @@
+//
+//  Proyecto_final_Diario_depresionTests.swift
+//  Proyecto_final_Diario_depresionTests
+//
+//  Created by Jorge Alberto de Jesus Cortinas Avila on 06/11/24.
+//
+
+import Testing
+@testable import Proyecto_final_Diario_depresion
+
+struct Proyecto_final_Diario_depresionTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
